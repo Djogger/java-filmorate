bcb54d16c4d5063cfaaad36efc7e38d4b8d1c47c
KriStiA <Valve>
2025-01-28 17:03:21 +0300

    Довести API до соответствия REST.

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -8,7 +8,8 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
 import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
 
-import java.util.Collection;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
@@ -49,8 +50,18 @@ public class UserController {
 
     @PutMapping("/{userId}/friends/{friendId}")
     @ResponseStatus(HttpStatus.OK)
-    public Collection<Long> addFriend(@PathVariable Long userId, @PathVariable Long friendId) {
-        return userService.addFriend(userId, friendId);
+    public Collection<Map<String, Long>> addFriend(@PathVariable Long userId, @PathVariable Long friendId) {
+        Collection<Long> response = userService.addFriend(userId, friendId);
+
+        Collection<Map<String, Long>> response2 = new ArrayList<>();
+        
+        for (Long id : response) {
+            Map<String, Long> friendMap = new HashMap<>();
+            friendMap.put("id", id);
+            response2.add(friendMap);
+        }
+
+        return response2;
     }
 
     @DeleteMapping("/{userId}/friends/{friendId}")
